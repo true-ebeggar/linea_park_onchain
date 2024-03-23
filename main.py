be4dd87e85a4eae9f9869e9b1785d506b56b07d2
Reilighost <reilighost@gmail.com>
2024-03-24 00:36:21 +0100

    добавил трейсинг ошибки

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import traceback
 import warnings
 from concurrent.futures import ThreadPoolExecutor, as_completed, wait, FIRST_COMPLETED
 from eth_account import Account as Acccount
@@ -24,102 +25,106 @@ def dummy_function_2(account_number, logger):
 
 
 def process_account(account_number, logger):
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=LegacyAPIWarning)
-        with DBSession() as session:
-            account = session.query(Account).get(account_number)
-            if not account:
-                logger.error(f"Account number: {account_number} not found. Exiting process.")
-                return
-            acc = Acccount.from_key(account.privatekey)
-            manager = LineaTxnManager(account.privatekey, account.proxystring)
+    try:
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=LegacyAPIWarning)
+            with DBSession() as session:
+                account = session.query(Account).get(account_number)
+                if not account:
+                    logger.error(f"Account number: {account_number} not found. Exiting process.")
+                    return
+                acc = Acccount.from_key(account.privatekey)
+                manager = LineaTxnManager(account.privatekey, account.proxystring)
 
-            tasks_action_map = {
-                # 'Task_101': dummy_function_1,
-                # 'Task_102': dummy_function_2,
-                # 'Task_103': dummy_function_1,
-                # 'Task_104': dummy_function_2,
-                # 'Task_201': dummy_function_1,
-                'Task_202': manager.mint_genesis_proof,
-                # 'Task_203': dummy_function_1,
-                # 'Task_204': dummy_function_2,
-                # 'Task_205': dummy_function_1,
-                # 'Task_206': dummy_function_2,
-                'Task_301': manager.mint_token_abyss_world,
-                'Task_302': manager.pictograph_wrap,
-                # 'Task_303': dummy_function_1,
-                'Task_304': manager.mint_satoshi_universe_battle_pass,
-                'Task_305': manager.check_in_yooldo,
-                'Task_401': manager.send_dmail_message,
-                'Task_402': manager.swap_on_gamic_dex,
-                'Task_403': manager.mint_emerald_enchanted_key,
-                'Task_404': manager.bit_avatar_wrap,
-                'Task_405': manager.read_on_curate,
-                'Task_406': manager.fire_moneygun_sending_me,
-                # 'Task_407': dummy_function_2,
-                # 'Task_408': dummy_function_1,
-                # 'Task_501': dummy_function_2,
-                'Task_502': manager.mint_tanuki_nft,
-                'Task_503': manager.mint2048,
-                # 'Task_504': dummy_function_1,
-                # 'Task_505': dummy_function_2,
-                # 'Task_506': dummy_function_1,
-                # 'Task_507': dummy_function_2,
-                'Task_508': manager.adopt_cat_call,
-                # 'Task_509': dummy_function_2,
-                'Task_601': manager.mint_NFTbadge,
-                'Task_602': manager.mint_battlemon,
-                # 'Task_603': dummy_function_2,
-                'Task_604': manager.mint_expedition_legacy,
-                # 'Task_605': dummy_function_1,
-                # 'Task_606': dummy_function_2,
-                # 'Task_607': dummy_function_1,
-                # 'Task_608': dummy_function_2,
-                # 'Task_609': dummy_function_1,
-                # 'Task_610': dummy_function_2,
-                # 'Task_611': dummy_function_1,
-                # 'Task_701': dummy_function_2,
-                # 'Task_702': dummy_function_1,
-                # 'Task_703': dummy_function_2,
-                # 'Task_704': dummy_function_1,
-                # 'Task_705': dummy_function_2,
-                # 'Task_706': dummy_function_1,
-                # 'Task_707': dummy_function_2,
-                # 'Task_708': dummy_function_1,
-                # 'Task_709': dummy_function_2,
-                # 'Task_710': dummy_function_1,
-                # 'Task_711': dummy_function_2,
-                # 'Task_801': dummy_function_1,
-                # 'Task_802': dummy_function_2,
-                # 'Task_803': dummy_function_1
-            }
+                tasks_action_map = {
+                    # 'Task_101': dummy_function_1,
+                    # 'Task_102': dummy_function_2,
+                    # 'Task_103': dummy_function_1,
+                    # 'Task_104': dummy_function_2,
+                    # 'Task_201': dummy_function_1,
+                    'Task_202': manager.mint_genesis_proof,
+                    # 'Task_203': dummy_function_1,
+                    # 'Task_204': dummy_function_2,
+                    # 'Task_205': dummy_function_1,
+                    # 'Task_206': dummy_function_2,
+                    'Task_301': manager.mint_token_abyss_world,
+                    'Task_302': manager.pictograph_wrap,
+                    # 'Task_303': dummy_function_1,
+                    'Task_304': manager.mint_satoshi_universe_battle_pass,
+                    'Task_305': manager.check_in_yooldo,
+                    'Task_401': manager.send_dmail_message,
+                    'Task_402': manager.swap_on_gamic_dex,
+                    'Task_403': manager.mint_emerald_enchanted_key,
+                    'Task_404': manager.bit_avatar_wrap,
+                    'Task_405': manager.read_on_curate,
+                    'Task_406': manager.fire_moneygun_sending_me,
+                    # 'Task_407': dummy_function_2,
+                    # 'Task_408': dummy_function_1,
+                    # 'Task_501': dummy_function_2,
+                    'Task_502': manager.mint_tanuki_nft,
+                    'Task_503': manager.mint2048,
+                    # 'Task_504': dummy_function_1,
+                    # 'Task_505': dummy_function_2,
+                    # 'Task_506': dummy_function_1,
+                    # 'Task_507': dummy_function_2,
+                    'Task_508': manager.adopt_cat_call,
+                    # 'Task_509': dummy_function_2,
+                    'Task_601': manager.mint_NFTbadge,
+                    'Task_602': manager.mint_battlemon,
+                    # 'Task_603': dummy_function_2,
+                    'Task_604': manager.mint_expedition_legacy,
+                    # 'Task_605': dummy_function_1,
+                    # 'Task_606': dummy_function_2,
+                    # 'Task_607': dummy_function_1,
+                    # 'Task_608': dummy_function_2,
+                    # 'Task_609': dummy_function_1,
+                    # 'Task_610': dummy_function_2,
+                    # 'Task_611': dummy_function_1,
+                    # 'Task_701': dummy_function_2,
+                    # 'Task_702': dummy_function_1,
+                    # 'Task_703': dummy_function_2,
+                    # 'Task_704': dummy_function_1,
+                    # 'Task_705': dummy_function_2,
+                    # 'Task_706': dummy_function_1,
+                    # 'Task_707': dummy_function_2,
+                    # 'Task_708': dummy_function_1,
+                    # 'Task_709': dummy_function_2,
+                    # 'Task_710': dummy_function_1,
+                    # 'Task_711': dummy_function_2,
+                    # 'Task_801': dummy_function_1,
+                    # 'Task_802': dummy_function_2,
+                    # 'Task_803': dummy_function_1
+                }
 
-            tasks_list = list(tasks_action_map.items())
-            random.shuffle(tasks_list)
+                tasks_list = list(tasks_action_map.items())
+                random.shuffle(tasks_list)
 
-            for column, action_function in tasks_list:
-                if getattr(account, column) == 0:
-                    logger.info(f'{column} is incomplete, fixing it now...')
-                    logger.info(
-                        f'Acc {acc.address} going to execute {action_function.__name__} function')
+                for column, action_function in tasks_list:
+                    if getattr(account, column) == 0:
+                        logger.info(f'{column} is incomplete, fixing it now...')
+                        logger.info(
+                            f'Acc {acc.address} going to execute {action_function.__name__} function')
 
-                    gas_gate()
-                    result = action_function()
+                        gas_gate()
+                        result = action_function()
 
-                    if result == 1:
-                        setattr(account, column, 1)
-                        session.commit()
-                        logger.success(f'{column} completed for account {acc.address}')
-                        sleep = random.randint(MIN_SLEEP, MAX_SLEEP)
-                        logger.info(f'Acc {acc.address} going to sleep for {sleep}-sec before continue')
-                        time.sleep(sleep)
+                        if result == 1:
+                            setattr(account, column, 1)
+                            session.commit()
+                            logger.success(f'{column} completed for account {acc.address}')
+                            sleep = random.randint(MIN_SLEEP, MAX_SLEEP)
+                            logger.info(f'Acc {acc.address} going to sleep for {sleep}-sec before continue')
+                            time.sleep(sleep)
+                        else:
+                            logger.error(f'Failed to complete {column} for account {acc.address}')
+                            continue
                     else:
-                        logger.error(f'Failed to complete {column} for account {acc.address}')
                         continue
-                else:
-                    continue
 
-            logger.warning(f'it look like all possible action is done for account {acc.address}')
+                logger.warning(f'it look like all possible action is done for account {acc.address}')
+    except Exception as e:
+        logger.critical(f"{e}")
+        logger.critical(f"traceback: {traceback.format_exc()}")
 
 
 def main():
